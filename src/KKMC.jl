bf444fb08c5aaddf1e0df7c9e18b98003934fd6c
Pere Giménez <gimenezfebrer@gmail.com>
2021-01-14 17:45:50 +0100

    KRRModel no longer stores the full kernel matrix

--- src/KKMC.jl
+++ src/KKMC.jl
@@ -23,7 +23,7 @@ export KRR, VectorData, fit!, predict!, SamplingMatrix, VectorSampler, KronVecto
 
 export MF, KMF, RGKMF, fit!, predict!, MatrixData
 
-export get_lscores, Leverage, train_test_pairs, LeverageReweight, transform
+export get_lscores, Leverage, train_test_pairs, LeverageReweight, transform, LKRRModel
 
 export MatrixToVectorData
 end # module
--- src/mlj_interface.jl
+++ src/mlj_interface.jl
@@ -1,42 +1,16 @@
 using MLJ,MLJModelInterface
-using Random, LinearAlgebra, StatsBase
+using Random, LinearAlgebra, StatsBase, Parameters
 import MLJBase
 
 const MMI = MLJModelInterface
 
-MMI.@mlj_model mutable struct KRRModel <: MLJModelInterface.Deterministic
-    mu::Float64 = 1::(_ > 0)
+@mlj_model mutable struct KRRModel <: MLJModelInterface.Deterministic
+    mu::Float64 = 1.0
     kernel::String = "linear"
-    kernel_matrix::Array{Float64,2} = zeros(1,1)
+    # kernel_matrix::Array{Float64,2} = zeros(2,2)
 end
 
-function MMI.fit(m::KRRModel,verbosity::Int,x,y,w=1)
-    # w = 1 ./ sqrt.(w*length(y))
-    x = x[1]
-    K = m.kernel_matrix[x,x]
-    K = reshape(K,(length(x),length(x)))
-    # W = diagm(w)
-    # K = W*K*W
-    # y = W*y
-    # fitresult = (x,W*inv(K+m.mu*I)*y)
-    fitresult = (x,inv(K+m.mu*I)*y)
-    cache = nothing
-    report = nothing
-    return (fitresult,cache,report)
-end
-
-function MMI.predict(m::KRRModel, fitresult, xnew) 
-    x = xnew[1]
-    samples, coef = fitresult
-    K = m.kernel_matrix[x,samples]
-    return K*coef
-end
-
-# function MMI.clean!(m::KRRModel) 
-    # return 1
-# end
-
-struct Leverage <: MLJ.ResamplingStrategy
+mutable struct Leverage <: MLJ.ResamplingStrategy
     s::Int64
     alpha::Float64
     probs::Array{Float64}
@@ -44,18 +18,39 @@ struct Leverage <: MLJ.ResamplingStrategy
 end
 
 # Keyword Constructor
-Leverage(; s=1, alpha=1, probs=1, folds=1) = Leverage(s,alpha,probs,folds)
+Leverage(; s=1, alpha=1.0, probs=[1.0], folds=1) = Leverage(s,alpha,probs,folds)
+
+function MMI.fit(m::KRRModel,verbosity::Int,X,y,w=1)
+    idx = y[1]
+    K = X[:,idx]
+    y = y[2]
+    fitresult = (idx,inv(K+m.mu*I)*y)
+    cache = nothing
+    report = nothing
+    return (fitresult,cache,report)
+end
+
+function MMI.predict(m::KRRModel, fitresult, xnew) 
+    samples, coef = fitresult
+    K = xnew[:,2:end]
+    K = xnew[:,samples]
+    return K*coef
+end
+
+# function MMI.clean!(m::KRRModel) 
+    # return 1
+# end
+
 
 
 function MLJBase.train_test_pairs(LS::Leverage, rows)
-    # train, test = partition(rows, LS.fraction_train,
-                          # shuffle=LS.shuffle, rng=holdout.rng)
     setlist = Tuple{Array{Int64,1},Array{Int64,1}}[]
     for i=1:LS.folds
         train = StatsBase.sample(rows, Weights(LS.probs[rows]),LS.s, replace=false)
         test = setdiff(rows,train)
         push!(setlist, (train,test))
     end
+    LS.s = LS.s + 1
     return setlist 
 end
 
@@ -79,12 +74,15 @@ function MLJ.transform(LR::LeverageReweight, fitresult, x::NamedTuple)
     idx = x[1]
     y = x[2]
     W = diagm(fitresult[idx])
-    return W*y
+    return (idx,W*y)
 end
 
 function MLJ.transform(LR::LeverageReweight, fitresult, K::Array{Float64,2})
-    W = diagm(fitresult[:])
-    return W*K*W
+    idx = Int.(K[:,1])
+    K = K[:,2:end]
+    Wl = diagm(fitresult[idx])
+    Wr = diagm(fitresult[:])
+    return Wl*K*Wr
 end
 
 MMI.metadata_pkg.(
