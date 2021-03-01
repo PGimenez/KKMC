50c05b51a4c1330caa432e0c5e1e94d48d8ba989
Pere Giménez <gimenezfebrer@gmail.com>
2021-03-01 17:20:09 +0100

    interface for AugmentedGaussianProcesses

---
+++ src/gp_interface.jl
@@ -0,0 +1,25 @@
+
+using MLJ,MLJModelInterface, MLJTuning
+using Random, LinearAlgebra, StatsBase, Parameters
+using AugmentedGaussianProcesses: GP, train!, predict_y
+using KernelFunctions: Kernel, EyeKernel, SqExponentialKernel
+import MLJBase
+
+const MMI = MLJModelInterface
+
+@with_kw mutable struct GaussianProcess <: MLJ.Deterministic
+    kernel::Kernel = EyeKernel()
+    noise::Float64 = 1.0
+    opt_noise::Bool = true
+end
+
+function MMI.fit(model::GaussianProcess, verbosity, X, y)
+    gp_model = GP(X,y,model.kernel,noise=model.noise,opt_noise=model.opt_noise,optimiser=false)
+    train!(gp_model)
+    return (gp_model,nothing,nothing)
+end
+
+function MMI.predict(model::GaussianProcess, fitresult, Xnew)
+    fitresult.likelihood.σ²[1] = model.noise
+    return  predict_y(fitresult,Xnew)
+end
